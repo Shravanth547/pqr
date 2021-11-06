224fabd396b7d604e9b71e30b5c01e3007303089
Ajay Navulla <ajay@ditcanada.com>
2021-11-06 14:45:37 -0400

    test

---
+++ src/testpackage/check.java
@@ -0,0 +1,12 @@
+package testpackage;
+
+public class check {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("one");
+		System.out.println("oneR");
+		System.out.println("oneRQ");
+	}
+
+}
